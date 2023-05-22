52ed8de22eb5b75b8e2429511d540c631b489501
ashwincc <ashwin567@proton.me>
2023-05-23 00:19:00 +0530

    corrected images channel path

--- container/local_test/train_local.sh
+++ container/local_test/train_local.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-
+# creating the folder structure
 mkdir -p test_dir/model
 mkdir -p test_dir/output
-mkdir -p test_dir/input/data
+mkdir -p local_test/test_dir/input/data/cfg
+mkdir -p local_test/test_dir/input/data/weights
 
+#<----------------------------------------------->
 cd test_dir/input/data
 wget https://www.dropbox.com/s/lbji5ho8b1m3op1/reduced_label_yolov6.zip?dl=1 -O dataset.zip
 
 unzip dataset.zip 
 
 mv reduced_label_yolov6/ dataset/
+#<----------------------------------------------->
 
+!wget https://github.com/meituan/YOLOv6/releases/download/0.3.0/yolov6s.pt
+!mv yolov6s.pt container/local_test/test_dir/input/data/weights/
+#<----------------------------------------------->
+
+
+
+!wget https://raw.githubusercontent.com/meituan/YOLOv6/main/configs/yolov6s_finetune.py
+!mv yolov6s_finetune.py container/local_test/test_dir/input/data/cfg/yolov6s_finetune.py
 docker run -it --ipc=host --gpus=all -v $(pwd)/test_dir:/opt/ml yolov6-sagemaker-training:v1 train
 
 
